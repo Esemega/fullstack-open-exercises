6d2bb4e0e6476777d15700f6000fff7b98145b25
Esemega <sogaral89@gmail.com>
2021-01-28 21:09:31 +0100

    refactoring code to remove definition of components inside the App component and unicafe step 3 DONE

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,44 +1,47 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({text}) => <h1>{text}</h1>
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const total = (good, neutral, bad) => good + neutral + bad;
+const average = (good, neutral, bad) => (good*1 + neutral*0 + bad*-1)/total(good, neutral, bad);
+const positive = (good, neutral, bad) => `${(good/total(good, neutral, bad))*100} %`;
+
+const Statistic = ({text, value}) => {
+  return(
+    <div>
+    {`${text} ${value}`}
+    </div>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  return(
+    <div>
+      <Statistic text={'good'} value={good}/>
+      <Statistic text={'neutral'} value={neutral}/>
+      <Statistic text={'bad'} value={bad}/>
+      <Statistic text={'all'} value={total(good, neutral, bad)}/>
+      <Statistic text={'average'} value={average(good, neutral, bad)}/>
+      <Statistic text={'positive'} value={positive(good, neutral, bad)}/>
+    </div>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-      
-  const Header = ({text}) => <h1>{text}</h1>
-  const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
-
-  const Statistic = ({text, value}) => {
-    return(
-      <div>
-      {`${text} ${value}`}
-      </div>
-    )
-  }
-
-  const increaseGood = () => setGood(good + 1);
-  const increaseNeutral = () => setNeutral(neutral + 1);
-  const increaseBad = () => setBad(bad + 1);
-  const total = () => good + neutral + bad;
-  const average = () => (good*1 + neutral*0 + bad*-1)/total();
-  const positive = () => `${(good/total())*100} %`;
-
   return (
     <div>
       <Header text = {'give feedback'}/>
-      <Button handleClick={increaseGood} text = {'good'}/>
-      <Button handleClick={increaseNeutral} text = {'neutral'}/>
-      <Button handleClick={increaseBad} text = {'bad'}/>
+      <Button handleClick={() => setGood(good + 1)} text = {'good'}/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text = {'neutral'}/>
+      <Button handleClick={() => setBad(bad + 1)} text = {'bad'}/>
       <Header text = {'statistics'}/>
-      <Statistic text={'good'} value={good}/>
-      <Statistic text={'neutral'} value={neutral}/>
-      <Statistic text={'bad'} value={bad}/>
-      <Statistic text={'all'} value={total()}/>
-      <Statistic text={'average'} value={average()}/>
-      <Statistic text={'positive'} value={positive()}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
